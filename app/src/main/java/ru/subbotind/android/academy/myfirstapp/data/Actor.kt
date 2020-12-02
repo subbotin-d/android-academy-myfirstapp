58c9f617762109b03a7fa884c1c034946e179325
dmitriysubbotin <subbotin.dmitriy93@gmail.com>
2020-12-02 22:50:04 +0700

    HW/4 - Переделал раздел Cast в список RecyclerView

--- app/src/main/java/ru/subbotind/android/academy/myfirstapp/data/Actor.kt
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/data/Actor.kt
@@ -3,7 +3,8 @@ package ru.subbotind.android.academy.myfirstapp.data
 import androidx.annotation.DrawableRes
 
 data class Actor(
+    val id: Int,
     val fullName: String,
     @DrawableRes
-    val avatar: Int
+    val avatar: Int? = null
 )
---
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/ui/moviedetails/adapter/ActorDiffUtilCallback.kt
@@ -0,0 +1,13 @@
+package ru.subbotind.android.academy.myfirstapp.ui.moviedetails.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import ru.subbotind.android.academy.myfirstapp.data.Actor
+
+class ActorDiffUtilCallback : DiffUtil.ItemCallback<Actor>() {
+
+    override fun areItemsTheSame(oldItem: Actor, newItem: Actor): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Actor, newItem: Actor): Boolean =
+        oldItem == newItem
+}
