babaadc89ae343aa461b2f35c5d21c732466c131
dmitriysubbotin <subbotin.dmitriy93@gmail.com>
2021-01-08 17:30:07 +0700

    HW/7 - Добавил репозиторий, который позволяет получить baseUrl для картинок
    Дополнительно:
    - используется кеширование урла
    - cache live time = 1 сутки

---
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/di/ConfigurationModule.kt
@@ -0,0 +1,42 @@
+package ru.subbotind.android.academy.myfirstapp.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import ru.subbotind.android.academy.myfirstapp.data.datasource.*
+import ru.subbotind.android.academy.myfirstapp.data.network.ConfigurationService
+import ru.subbotind.android.academy.myfirstapp.data.repository.BaseImageUrlRepository
+import ru.subbotind.android.academy.myfirstapp.data.repository.BaseImageUrlRepositoryImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+interface ConfigurationModule {
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideConfigurationService(retrofit: Retrofit): ConfigurationService =
+            retrofit.create()
+    }
+
+    @Binds
+    @Singleton
+    fun bindsBaseUrlRemoteDataSource(baseUrlRemoteDataSource: BaseUrlRemoteDataSourceImpl): BaseUrlRemoteDataSource
+
+    @Binds
+    @Singleton
+    fun bindsBaseUrlLocalDataSource(baseUrlLocalDataSource: BaseUrlLocalDataSourceImpl): BaseUrlLocalDataSource
+
+    @Binds
+    @Singleton
+    fun bindsConfigurationLastLoadingTimeDataSource(configurationLastLoadingTimeDataSource: ConfigurationLastLoadingTimeDataSourceImpl): ConfigurationLastLoadingTimeDataSource
+
+    @Binds
+    @Singleton
+    fun bindsBaseUrlRepository(baseImageUrlRepository: BaseImageUrlRepositoryImpl): BaseImageUrlRepository
+}
