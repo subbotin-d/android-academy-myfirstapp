6781fe277eab78be67ce7e4c9adae518da858717
dmitriysubbotin <subbotin.dmitriy93@gmail.com>
2021-01-17 23:12:01 +0700

    HW/7 - Добавил везде ааннотации SerializedName

--- app/src/main/java/ru/subbotind/android/academy/myfirstapp/data/model/GenreModel.kt
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/data/model/GenreModel.kt
@@ -1,7 +1,11 @@
 package ru.subbotind.android.academy.myfirstapp.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ru.subbotind.android.academy.myfirstapp.domain.entity.Genre
 
 @Serializable
-data class GenreModel(val genres: List<Genre>)
+data class GenreModel(
+    @SerialName("genres")
+    val genres: List<Genre>
+)
