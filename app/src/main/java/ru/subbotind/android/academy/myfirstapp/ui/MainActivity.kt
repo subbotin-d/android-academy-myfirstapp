4378acc15cbf55a5d5e3023f6edcced308416537
dmitriysubbotin <subbotin.dmitriy93@gmail.com>
2021-02-13 01:24:31 +0700

    HW/10 - Добавил deepLink на открытие экрана деталей фильма

--- app/src/main/java/ru/subbotind/android/academy/myfirstapp/ui/MainActivity.kt
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.subbotind.android.academy.myfirstapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.work.WorkRequest
 import dagger.hilt.android.AndroidEntryPoint
 import ru.subbotind.android.academy.myfirstapp.R
+import ru.subbotind.android.academy.myfirstapp.ui.moviedetails.MovieDetailsFragment
 import ru.subbotind.android.academy.myfirstapp.background.WorkController
 import ru.subbotind.android.academy.myfirstapp.ui.movielist.MovieListFragment
 import javax.inject.Inject
@@ -25,12 +28,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            movieListFragment = MovieListFragment.newInstance()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainer, movieListFragment, MovieListFragment.TAG)
-                .commit()
+            openMoviesScreen()
+            intent?.let(::handleIntent)
         }
 
         workController.startWork(movieSynchronizationRequest)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_VIEW -> {
+                val movieId = intent.data?.lastPathSegment?.toIntOrNull()
+                movieId?.let {
+                    openMovieScreen(movieId)
+                }
+            }
+        }
+    }
+
+    private fun openMoviesScreen() {
+        val movieListFragment = MovieListFragment.newInstance()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, movieListFragment, MovieListFragment.TAG)
+            .commit()
+    }
+
+    private fun openMovieScreen(movieId: Int) {
+        supportFragmentManager.popBackStack(
+            MovieListFragment.TAG,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
+
+        val movieDetailsFragment = MovieDetailsFragment.newInstance(movieId)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, movieDetailsFragment, MovieListFragment.TAG)
+            .addToBackStack(MovieDetailsFragment.TAG)
+            .commit()
     }
 }
