146c6a103d6490edcd9c4c2f883ac0d87ee503ee
dmitriysubbotin <subbotin.dmitriy93@gmail.com>
2021-02-13 11:58:05 +0700

    HW/10 - Реализовал создание канала при старте активити

--- app/src/main/java/ru/subbotind/android/academy/myfirstapp/ui/MainActivity.kt
+++ app/src/main/java/ru/subbotind/android/academy/myfirstapp/ui/MainActivity.kt
@@ -1,21 +1,29 @@
 package ru.subbotind.android.academy.myfirstapp.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 import androidx.work.WorkRequest
 import dagger.hilt.android.AndroidEntryPoint
 import ru.subbotind.android.academy.myfirstapp.R
-import ru.subbotind.android.academy.myfirstapp.ui.moviedetails.MovieDetailsFragment
 import ru.subbotind.android.academy.myfirstapp.background.WorkController
+import ru.subbotind.android.academy.myfirstapp.di.ChannelIdQualifier
+import ru.subbotind.android.academy.myfirstapp.ui.moviedetails.MovieDetailsFragment
 import ru.subbotind.android.academy.myfirstapp.ui.movielist.MovieListFragment
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var movieListFragment: MovieListFragment
+    @ChannelIdQualifier
+    @Inject
+    lateinit var channelId: String
 
     @Inject
     lateinit var movieSynchronizationRequest: WorkRequest
@@ -56,9 +64,9 @@ class MainActivity : AppCompatActivity() {
     private fun openMoviesScreen() {
         val movieListFragment = MovieListFragment.newInstance()
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, movieListFragment, MovieListFragment.TAG)
-            .commit()
+        supportFragmentManager.commit {
+            add(R.id.fragmentContainer, movieListFragment, MovieListFragment.TAG)
+        }
     }
 
     private fun openMovieScreen(movieId: Int) {
@@ -69,9 +77,29 @@ class MainActivity : AppCompatActivity() {
 
         val movieDetailsFragment = MovieDetailsFragment.newInstance(movieId)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, movieDetailsFragment, MovieListFragment.TAG)
-            .addToBackStack(MovieDetailsFragment.TAG)
-            .commit()
+        supportFragmentManager.commit {
+            add(R.id.fragmentContainer, movieDetailsFragment, MovieListFragment.TAG)
+            addToBackStack(MovieDetailsFragment.TAG)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = getString(R.string.channel_name_text)
+            val channelDescription = getString(R.string.channel_description_text)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            NotificationManagerCompat.from(applicationContext).createNotificationChannel(channel)
+        }
     }
 }
